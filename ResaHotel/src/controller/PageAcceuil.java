73b123e1680a38ad70cdec159bc22634fe1262c8
KevinSinna <45667777+KevinSinna@users.noreply.github.com>
2019-04-02 20:53:19 +0200

    Page intervace graphique connecter entre elle , lancement des pas effectuer via les controller

---
+++ ResaHotel/src/controller/PageAcceuil.java
@@ -0,0 +1,74 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class PageAcceuil {
+
+	Stage stage; 
+	Parent root;
+
+    @FXML
+    private Button btnAccueil;
+
+    @FXML
+    private Button btnGestch;
+
+    @FXML
+    private Button btnGestres;
+
+    @FXML
+    private Button btnClient;
+
+    @FXML
+    private Button btnFact;
+
+
+public void handleButtonAction(ActionEvent event) throws IOException {
+     
+
+if(event.getSource()==btnAccueil){
+    	stage=(Stage) btnAccueil.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("/View/accueil.fxml"));
+        Scene accueil = new Scene(root);
+ 	   stage.setScene(accueil);
+ stage.show();
+   }
+
+if(event.getSource()==btnGestch){
+	stage=(Stage) btnGestch.getScene().getWindow();
+    root = FXMLLoader.load(getClass().getResource("/View/gestionchambre.fxml"));
+    Scene gestionchambre = new Scene(root);
+	   stage.setScene(gestionchambre);
+stage.show();
+}
+if(event.getSource()==btnGestres){
+	stage=(Stage) btnGestres.getScene().getWindow();
+    root = FXMLLoader.load(getClass().getResource("/View/gestionreservation.fxml"));
+    Scene gestionres = new Scene(root);
+	   stage.setScene(gestionres);
+stage.show();
+}
+if(event.getSource()==btnClient){
+	stage=(Stage) btnClient.getScene().getWindow();
+    root = FXMLLoader.load(getClass().getResource("/View/client.fxml"));
+    Scene client = new Scene(root);
+	   stage.setScene(client);
+stage.show();
+}
+if(event.getSource()==btnFact){
+	stage=(Stage) btnFact.getScene().getWindow();
+    root = FXMLLoader.load(getClass().getResource("/View/facturation.fxml"));
+    Scene facture = new Scene(root);
+	   stage.setScene(facture);
+stage.show();
+}
+}
+}
--- ResaHotel/src/dao/main.java
+++ ResaHotel/src/dao/main.java
@@ -1,19 +1,34 @@
 package dao;
 
 import java.sql.Connection;
-
 import com.mysql.jdbc.PreparedStatement;
 import com.mysql.jdbc.ResultSet;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import model.Chambre;
 
-public class main {
+public class main extends Application {
 
+	private static Stage stage;
+	private AnchorPane root;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Chambre kev = new Chambre(43,333);
-		kev.Ajout();
-		
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		stage=arg0;
+		root = FXMLLoader.load(getClass().getResource("/View/accueil.fxml"));
+		Scene scene = new Scene(root);
+        stage.setScene(scene);
+stage.show();
 	}
 
 }
