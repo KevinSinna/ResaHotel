5f9cde8bad3981ee59b00c615487fa0d1975e135
KevinSinna <45667777+KevinSinna@users.noreply.github.com>
2019-04-17 10:58:39 +0200

    Ajout TableView Dans PageClient.

--- ResaHotel/src/controller/PageClient.java
+++ ResaHotel/src/controller/PageClient.java
@@ -12,6 +12,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import model.Client;
@@ -19,6 +21,17 @@ import model.Client;
 public class PageClient implements Observer {
 	Stage stage; 
 	Parent root;
+	@FXML
+	private TableView<?> TabViewCleint;
+
+	@FXML
+	private TableColumn<?, ?> ColIdCleint;
+
+	@FXML
+	private TableColumn<?, ?> ColNom;
+
+	@FXML
+	private TableColumn<?, ?> ColPrenom;	
 
     @FXML
     private Button btnAccueil;
